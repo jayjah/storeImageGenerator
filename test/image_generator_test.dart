9f9277abc003828c259e2cc591b3964ec695ea38
Markus Krebs <markuskrebs93@gmail.com>
2021-07-31 01:08:38 +0200

    added tests

---
+++ test/image_generator_test.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+import 'package:dcli/dcli.dart';
+import 'package:storeImageGenerator/devices.dart' as devices;
+import 'package:storeImageGenerator/image_generator.dart' as gen;
+import 'package:test/test.dart';
+
+Future<void> main() async {
+  test('Generate store icons', () async {
+    const outputPath = './output/';
+    const inputPath = './test';
+    const imagesToGenerate = [...devices.androidDevices, ...devices.iosDevices];
+
+    gen.createDirectory(outputPath);
+    expect(Directory(outputPath).existsSync(), true);
+
+    final inputImages =
+        find('*.jpg', workingDirectory: inputPath, types: [Find.file]).toList();
+
+    await gen.convertImages(imagesToGenerate, inputImages, outputPath);
+
+    final outputImages =
+        find('*.*', workingDirectory: outputPath, types: [Find.file]).toList();
+
+    expect(imagesToGenerate.length, outputImages.length);
+  });
+}
